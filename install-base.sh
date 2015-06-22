ab28f72a5af29d482acce6578e20ffdd3e3ee7f3
Steffen Fuchs <st.fuchs.tr@gmail.com>
2015-06-22 11:27:03 +0200

    fixed auto source

--- install-base.sh
+++ install-base.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -f $HOME/.bash_aliases ]; then
     echo "Creating .bash_aliases"
     echo '#!/bin/sh' > $HOME/.bash_aliases
     echo 'source $HOME/git/ubuntu_config/bash/bashrc' >> $HOME/.bash_aliases
-    source $HOME/.bashrc
+    . $HOME/.bashrc
 fi
 
 # config terminator
